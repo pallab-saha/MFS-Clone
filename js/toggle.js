8aa9a265b79a50206525308771098c5af01ee3ba
pallab-saha <spallabsaha16@gmail.com>
2025-05-22 14:30:29 +0600

    kichu ekta sesh

--- js/toggle.js
+++ js/toggle.js
@@ -1,3 +1,22 @@
+
+document.getElementById('show-cash-out').addEventListener('click', function (event) {
+    //hiding add money form
+
+    //document.getElementById('add-money-form').style.display = 'none';
+    document.getElementById('add-money-form').classList.add('hidden');
+    //showing cash out form
+
+    //document.getElementById('cash-out-form').style.display = 'block';
+    document.getElementById('cash-out-form').classList.remove('hidden');
+})
+
 document.getElementById('show-add-money').addEventListener('click', function (event) {
-    const show
+    //hiding cashout form
+
+    //document.getElementById('cash-out-form').style.display = 'none';
+    document.getElementById('cash-out-form').classList.add('hidden');
+    //showing add money form
+
+    //document.getElementById('add-money-form').style.display = 'block';
+    document.getElementById('cash-out-form').classList.remove('hidden');
 })
